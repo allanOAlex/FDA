a318df153b95b8a8a3ba7123b38b0ccc32e6a393
allanOAlex <allan.alex0803@gmail.com>
2023-06-29 19:46:57 +0300

    Can now reset and close modal successfully after updating employee salary. (Got Swamped for a bit.)

--- src/Web/TB.Mvc/wwwroot/js/updateEmployeeSalary.js
+++ src/Web/TB.Mvc/wwwroot/js/updateEmployeeSalary.js
@@ -1,20 +1,17 @@
-﻿$(document).ready(function () {
-
+﻿$(function () {
     var Id;
     var OldSalary;
 
     $(document).on('click', '.edit-employee', function () {
         Id = $(this).data('emp-id');
-        $('#empID').val(Id); 
+        $('#empID').val(Id);
 
         OldSalary = $(this).closest('tr').find('.salary-cell').text();
         $('#currentSalary').text(OldSalary);
         $('#currentSalary').val(OldSalary);
-        
-
     });
 
-    $('#updateEmpSalaryForm').submit(function (e) {
+    $('#updateEmpSalaryForm').on('submit', function (e) {
         e.preventDefault();
 
         $.ajax({
@@ -23,26 +20,29 @@
             data: $(this).serialize(),
             success: function (response) {
                 if (response.successful) {
-                    resetAndCloseUpdateEmployeeSalaryModal()
-
-                    Swal.fire('Success!', 'Salary updated successfully from ' + response.oldSalary + ' to ' + response.newSalary, 'success');
-
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Salary updated successfully from ' + response.oldSalary + ' to ' + response.newSalary,
+                        icon: 'success'
+                    }).then(function () {
+                        resetForm('updateEmpSalaryForm');
+                        closeModal('updateEmpSalaryModal');
+                    });
                 } else {
-                    Swal.fire('Error!', 'Salary update failes. Please contact system admin.', 'error');
+                    Swal.fire('Error!', 'Salary update failed. Please contact system admin.', 'error');
                 }
             },
             error: function () {
                 Swal.fire('Request Failed!', 'Please contact system admin.', 'error');
             }
         });
-
-        
-
-
-    })
-
-
-
-
+    });
 });
-    
+
+
+function closeModal(modalId) {
+    $('#' + modalId).modal('hide');
+}
+
+
+
